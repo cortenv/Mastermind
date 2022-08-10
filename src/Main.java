40cda192501d3c3fe43f5ef5b9ac0c81bcc545e7
cortenv <vera.corten@hotmail.com>
2022-08-10 16:51:41 +0200

    loops

--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,68 @@
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        boolean place = false;
+        boolean colorInput = false;
+
         Random rand = new Random();
         String [] colors = {"red", "blue", "green", "gray", "pink", "yellow"};
-        System.out.println(colors [rand.nextInt(colors.length)]);
-        System.out.println(colors [rand.nextInt(colors.length)]);
-        System.out.println(colors [rand.nextInt(colors.length)]);
-        System.out.println(colors [rand.nextInt(colors.length)]);
-
-        Scanner input = new Scanner(System.in);
-        String userI = input.nextLine();
-
-        if (boolean userI =) {
-            System.out.println("Correct!");
-        } else if (userI = farbe) {                             // farbe
-            System.out.println("Correct color");
-        } else if (userI = ort) {                               // ort
-            System.out.println("Correct place");
-        } else {
-            System.out.println("Try again");
+        for (int i = 0; i < 4; i++) {
+            System.out.println(colors [rand.nextInt(colors.length)]);
         }
 
+        for (int i = 0; i < 1; i++) {
+            Scanner input = new Scanner(System.in);
+
+
+            String [] userInputs = new String [4] ;
+            for (int j = 0; j < 4; j++) {
+                System.out.println("Insert color");
+                userInputs [j] = input.nextLine();
+            }
+
+            if (userI.equals) {              // userI??
+                System.out.println("Correct!");
+            } else if (colorInput == true) {
+                System.out.println("Correct color");
+            } else if (place == true) {
+                System.out.println("Correct place");
+            } else {
+                System.out.println("Wrong");
+            }
+
+        }
 
         // colors = array / colorScan = scanner /
-                    // userI = variable
         // Yes No if Schleife
         // if richtige Farbe+Ort
         // int colorScan = scanner1.nextInt();
-
-        Scanner colorScan = new Scanner(System.in);
-        String userC = colorScan.nextLine();
 
         for (int i = 0; i < 6; i++) {
 
         switch(colors[i]) {
             case "red":
-                System.out.print(" red");
+                System.out.println(" red");
                 break;
             case "blue":
-                System.out.print(" blue");
+                System.out.println(" blue");
                 break;
             case "green":
-                System.out.print(" green");
+                System.out.println(" green");
                 break;
             case "gray":
-                System.out.print(" gray");
+                System.out.println(" gray");
                 break;
             case "pink":
-                System.out.print(" pink");
+                System.out.println(" pink");
                 break;
             case "yellow":
-                System.out.print(" yellow");
+                System.out.println(" yellow");
                 break;
             default:
-                System.out.print(" no color");
+                System.out.println(" no color");
         }
         }
 
